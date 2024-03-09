e617dafad7f9d629d758b7ab6417e9543c217826
Shubham Bhatyappan <100052872+Shubhat999@users.noreply.github.com>
2024-03-10 02:50:40 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="mlproject",
     version="0.0.1",
-    author="Krish",
-    author_email="krishnaik06@gmail.com",
+    author="Shubham",
+    author_email="Shubhat999@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
